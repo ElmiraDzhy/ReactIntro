17612bf46a4893265635261480cb2b99e4e8bf52
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-18 10:17:25 +0300

    Add code example of react

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 console.log(React);
 console.log(ReactDOM);
 
-const h1 = React.createElement('h1', {}, 'Hello React!');
+const state = `Hello`;
 
-
+const h1 = React.createElement('h1', {}, `${state}, React!`);
 const root = document.querySelector('#root');
-
 ReactDOM.render(h1, root);
+
+const component = (data) => {
+    const h2 = React.createElement('h2', {}, 'wefwefwe')
+    const p = React.createElement('p', {}, 'regewrgegergwe')
+
+    return React.createElement('div', {}, `Hello, ${data.heading}`, h2, p);
+}
+
+const elem = React.createElement(component, {heading: 'Text'}, `${state}, React!`);
+const elem2 = React.createElement(component, {heading: 'React'}, `${state}, React!`);
+const elem3 = React.createElement(component, {heading: 'Ann'}, `${state}, React!`);
+const elem4 = React.createElement(component, {heading: 'Tom'}, `${state}, React!`);
+const elem5 = React.createElement(component, {heading: 'React'}, `${state}, React!`);
+
+const fragment = React.createElement('div', {}, elem, elem2, elem3, elem4, elem5);
+
+
+
+ReactDOM.render(fragment, root);
+
