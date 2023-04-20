6d6b71e541a8d67bc046bd831df4120636b90480
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-20 10:43:29 +0300

    Add example of update components methods

--- src/components/Tree/index.jsx
+++ src/components/Tree/index.jsx
@@ -13,6 +13,21 @@ export default class Tree extends Component {
 		console.log("component did mount");
 	}
 
+	componentDidUpdate() {
+		console.log("component did update");
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log("should component update");
+		//если возвр true - компонент требует обновления
+
+		if (this.state.checked === nextState.checked) {
+			return false;
+		}
+
+		return true;
+	}
+
 	onClickHandler = () => {
 		this.setState({
 			checked: !this.state.checked,
