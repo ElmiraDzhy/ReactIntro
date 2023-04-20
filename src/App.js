748e93a9d0f018c945657bd4420f89b434d7064c
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-20 09:29:09 +0300

    Add task resolve (sort arrUsers)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
-import Box from './components/Box';
+import React from "react";
+import Aloha from "./components/Aloha";
 
-const App = (props) => {
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-  return (
-    <>
-      <Box/>
-    </>
-  );
+		this.state = {
+			users: [
+				{
+					name: "John",
+					age: 12,
+				},
+				{
+					name: "Ann",
+					age: 23,
+				},
+				{
+					name: "Kate",
+					age: 15,
+				},
+				{
+					name: "Tom",
+					age: 19,
+				},
+				{
+					name: "Vasy",
+					age: 27,
+				},
+				{
+					name: "Lola",
+					age: 30,
+				},
+				{
+					name: "Poul",
+					age: 17,
+				},
+				{
+					name: "George",
+					age: 18,
+				},
+				{
+					name: "Dominick",
+					age: 43,
+				},
+			],
+		};
+	}
+
+	sortUsers = () => {
+		const arr = [...this.state.users];
+		arr.sort((a, b) => {
+			return a.name < b.name ? -1 : 1;
+		});
+
+		this.setState({
+			users: arr,
+		});
+	};
+
+	render() {
+		const { users } = this.state;
+
+		return (
+			<>
+				<button onClick={this.sortUsers}>Sort Users</button>
+				{users.map((u) => (
+					<Aloha name={u.name} />
+				))}
+			</>
+		);
+	}
 }
 
 export default App;
+
