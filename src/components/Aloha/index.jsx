f5116aa57e159a987acdf9987195a2dbd3d415b2
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-20 07:43:50 +0300

    Fix greeting bug

--- src/components/Aloha/index.jsx
+++ src/components/Aloha/index.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
+import React from "react";
 
 class Aloha extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLeaving: false,
+		};
+	}
 
-  constructor(props){
-    super(props);
-    this.state = {
-      isLeaving: true,
-      welcome: 'Hello', 
-    }
-  }
+	greeting = () => {
+		const { isLeaving } = this.state;
+		this.setState({
+			isLeaving: !isLeaving,
+		});
+	};
 
-  greeting = () => {
-    const {isLeaving} = this.state;
-    this.setState({
-      isLeaving: !isLeaving,
-      welcome: isLeaving ? "Bye" : "Hello",
-    });
-  }
-
-
-  render(){
-    const {name} = this.props;
-    return (
-      <div>
-        {name}, {this.state.welcome}
-        <button onClick={this.greeting}>Change</button>
-      </div>
-    )
-  }
-
+	render() {
+		const { name } = this.props;
+		return (
+			<div>
+				{name}, {this.state.isLeaving ? "Bye" : "Hello"}
+				<button onClick={this.greeting}>Change</button>
+			</div>
+		);
+	}
 }
 
-export default Aloha
+export default Aloha;
+
