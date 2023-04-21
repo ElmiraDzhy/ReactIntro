28d612542bd77c07271b39828963f732a62cace6
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-21 10:14:50 +0300

    Add universal handler method

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { format } from "date-fns";
 
 export default class ToDoList extends Component {
 	constructor(props) {
@@ -6,6 +7,8 @@ export default class ToDoList extends Component {
 
 		this.state = {
 			toDoItem: "",
+			isDone: false,
+			time: new Date(0, 0, 0, 0, 0, 0, 0),
 		};
 	}
 
@@ -14,20 +17,61 @@ export default class ToDoList extends Component {
 		console.log(e.target.children[0].value);
 	};
 
-	changeHandler = (e) => {
-		this.setState({
-			toDoItem: e.target.value,
-		});
+	generalHandler = ({ target }) => {
+		switch (target.type) {
+			case "text": {
+				this.setState({
+					toDoItem: target.value,
+				});
+				break;
+			}
+
+			case "checkbox": {
+				this.setState(() => ({
+					isDone: target.checked,
+				}));
+				break;
+			}
+
+			case "datetime-local": {
+				this.setState({
+					time: new Date(target.value),
+				});
+				break;
+			}
+
+			default:
+		}
 	};
 
 	render() {
+		const { toDoItem, time, isDone } = this.state;
+
 		return (
 			<>
 				<form onSubmit={this.submitHandler}>
 					<input
+						name="toDoItem"
 						type="text"
-						value={this.state.toDoItem}
-						onChange={this.changeHandler}
+						value={toDoItem}
+						onChange={this.generalHandler}
+					/>
+
+					<label>
+						isDone?
+						<input
+							name="check"
+							type="checkbox"
+							checked={isDone}
+							onChange={this.generalHandler}
+						/>
+					</label>
+
+					<input
+						name="time"
+						type="datetime-local"
+						value={format(time, "yyyy-MM-dd hh:mm")}
+						onChange={this.generalHandler}
 					/>
 					<button>Submit</button>
 				</form>
