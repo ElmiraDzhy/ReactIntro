723a0651f858b7b8378e90aa3253dee50cac4463
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-24 10:48:23 +0300

    Add defaultProps example

--- src/components/Aloha/index.jsx
+++ src/components/Aloha/index.jsx
@@ -1,51 +1,57 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
 class Aloha extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLeaving: false,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLeaving: false,
+    };
+  }
 
-	greeting = () => {
-		const { isLeaving } = this.state;
-		this.setState({
-			isLeaving: !isLeaving,
-		});
-	};
+  greeting = () => {
+    const { isLeaving } = this.state;
+    this.setState({
+      isLeaving: !isLeaving,
+    });
+  };
 
-	delete = () => {
-		const {
-			user: { id },
-			deleteCallback,
-		} = this.props;
-		deleteCallback(id);
-	};
+  delete = () => {
+    const {
+      user: { id },
+      deleteCallback,
+    } = this.props;
+    deleteCallback(id);
+  };
 
-	render() {
-		const { name } = this.props.user;
-		return (
-			<li>
-				{name}, {this.state.isLeaving ? "Bye" : "Hello"}
-				<button onClick={this.greeting}>Change</button>
-				<button onClick={this.delete}>x</button>
-			</li>
-		);
-	}
+  render() {
+    const { name } = this.props.user;
+    return (
+      <li>
+        {name}, {this.state.isLeaving ? 'Bye' : 'Hello'}
+        <button onClick={this.greeting}>Change</button>
+        <button onClick={this.delete}>x</button>
+      </li>
+    );
+  }
 }
 
 const PropUser = {
-	name: PropTypes.string.isRequired,
-	id: PropTypes.number.isRequired,
-}
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+};
 
 Aloha.propTypes = {
-	user: PropTypes.shape( PropUser ).isRequired,
-	deleteCallback: PropTypes.func.isRequired,
-	
-}
+  user: PropTypes.shape(PropUser).isRequired,
+  deleteCallback: PropTypes.func,
+};
+
+Aloha.defaultProps = {
+  deleteCallback: () => {},
+  user: {
+    name: 'Anonim',
+    id: 123,
+  },
+};
 
 export default Aloha;
-
