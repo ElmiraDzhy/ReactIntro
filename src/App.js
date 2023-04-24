cdaf130213b5112b3e5f66315df7045e97c45529
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-24 10:56:09 +0300

    Resolve propTypes task

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from "react";
-import Aloha from './components/Aloha'
+import React from 'react';
+import Aloha from './components/Aloha';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isRendering: true,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRendering: true,
+    };
+  }
 
-	onClick = () => {
-		this.setState({
-			isRendering: false,
-		});
-	};
+  onClick = () => {
+    this.setState({
+      isRendering: false,
+    });
+  };
 
-	render() {
-		const { isRendering } = this.state;
+  render() {
+    const { isRendering } = this.state;
 
-		return (
-			<>
-				<button onClick={this.onClick}>Kill me</button>
-				{ <Aloha user={ {name: '', id: 1}} deletedCallback={()=> {}} />}
-			</>
-		);
-	}
+    return (
+      <>
+        <button onClick={this.onClick}>Kill me</button>
+        {<Aloha />}
+      </>
+    );
+  }
 }
 
 export default App;
-
--- src/components/ToDoList/ToDoItem/index.jsx
+++ src/components/ToDoList/ToDoItem/index.jsx
@@ -1,65 +1,81 @@
-import React from "react";
-import { format } from "date-fns";
+import React from 'react';
+import { format } from 'date-fns';
 import styles from './ToDoItem.module.css';
+import PropTypes from 'prop-types';
 
 class ToDoItem extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			editMode: false,
-		};
-	}
-	onChange = (e) => {
-		this.props.changeIsDone(e.target.checked, this.props.data.id);
-	};
-
-	onClick = () => {
-		this.props.removeListItem(this.props.data.id);
-	};
-
-	changeView = () => {
-		const { editMode } = this.state;
-		this.setState({
-			editMode: !editMode,
-		});
-	};
-
-  bodyChangeHandler = ( { target: { value } } ) => {
-    const { changeText, data: { id } } = this.props;
-    changeText( value, id );
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: false,
+    };
+  }
+  onChange = e => {
+    this.props.changeIsDone(e.target.checked, this.props.data.id);
   };
 
+  onClick = () => {
+    this.props.removeListItem(this.props.data.id);
+  };
 
-
-	render() {
-		const {
-			data: { isDone, time, toDoItem},
-    } = this.props;
-    
+  changeView = () => {
     const { editMode } = this.state;
-    
-		const body = editMode ? <input value={toDoItem} onChange={this.bodyChangeHandler}/> : toDoItem;
+    this.setState({
+      editMode: !editMode,
+    });
+  };
 
-		return (
-			
-				<li className={styles.container}>
-					<input
-						type="checkbox"
-						checked={isDone}
-						onChange={this.onChange}
-          />
-          
-          <span>{body}</span> -
-          <span>{format(time, "yyyy-MM-dd hh:mm")}</span>
-				  <button className={styles.delBtn} onClick={this.onClick}>delete</button>
-				  <button onClick={this.changeView}>edit</button>
-			
-					
-				</li>
-			
-		);
-	}
+  bodyChangeHandler = ({ target: { value } }) => {
+    const {
+      changeText,
+      data: { id },
+    } = this.props;
+    changeText(value, id);
+  };
+
+  render() {
+    const {
+      data: { isDone, time, toDoItem },
+    } = this.props;
+
+    const { editMode } = this.state;
+
+    const body = editMode ? (
+      <input value={toDoItem} onChange={this.bodyChangeHandler} />
+    ) : (
+      toDoItem
+    );
+
+    return (
+      <li className={styles.container}>
+        <input type="checkbox" checked={isDone} onChange={this.onChange} />
+        <span>{body}</span> -<span>{format(time, 'yyyy-MM-dd hh:mm')}</span>
+        <button className={styles.delBtn} onClick={this.onClick}>
+          delete
+        </button>
+        <button onClick={this.changeView}>edit</button>
+      </li>
+    );
+  }
 }
 
-export default ToDoItem;
+const dataProp = {
+  isDone: PropTypes.bool.isRequired,
+  time: PropTypes.data.isRequired,
+  toDoItem: PropTypes.string.isRequired,
+};
 
+ToDoItem.propTypes = {
+  data: PropTypes.shape(dataProp).isRequired,
+  changeIsDone: PropTypes.func.isRequired,
+  removeListItem: PropTypes.func.isRequired,
+  changeText: PropTypes.func.isRequired,
+};
+
+ToDoItem.defaultProps = {
+  changeIsDone: () => {},
+  removeListItem: () => {},
+  changeText: () => {},
+};
+
+export default ToDoItem;
